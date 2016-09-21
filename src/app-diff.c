bbec9eec7ac3a7e3c886c7ab6e609783ec843bda
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-21 16:08:01 +0000

    try scoring files
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/app-diff.c
+++ src/app-diff.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include "api.h"
 
 int main(int argc, const char *argv[])
 {
-	printf("NOT IMPLEMENTED YET");
+	printf("NOT IMPLEMENTED YET\n");
+	if (argc != 3) {
+		return;
+	}
+	const char *first = argv[1];
+	const char *second = argv[2];
+
+	printf("> COMPARING %s %s\n> > Score is %f\n", first, second,
+	       diff_score_files(first, second));
+
 	return 0;
 }
