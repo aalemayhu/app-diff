9f00d1f854eb0fef15f7e1a035dac764688cd632
Alexander Alemayhu <alexander@alemayhu.com>
2016-09-21 16:27:09 +0000

    move file comparison into own function
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- src/app-diff.c
+++ src/app-diff.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include "api.h"
 
+void compare_files(const char *argv);
+
 int main(int argc, const char *argv[])
 {
 	printf("NOT IMPLEMENTED YET\n");
-	if (argc != 3) {
-		return;
+	if (argc == 3) {
+		compare_files(argv);
 	}
+
+	return 0;
+}
+
+void compare_files(const char *argv)
+{
 	const char *first = argv[1];
 	const char *second = argv[2];
 
 	printf("> COMPARING %s %s\n> > Score is %f\n", first, second,
 	       diff_score_files(first, second));
-
-	return 0;
 }
